df9664b2d9b3af4632e9942614d4620af900321f
Simon Hengel <sol@typeful.net>
2022-03-28 10:21:58 +0700

    Add names to workflows

--- generate/selftest.ts
+++ generate/selftest.ts
@@ -5,7 +5,6 @@ import { versions } from './versions';
 for (const [name, supported] of Object.entries(versions)) {
   const file = `.github/workflows/selftest.${name}.yml`;
   const yaml = YAML.parse(fs.readFileSync(file, 'utf8'));
-  yaml.jobs.build.strategy.matrix.os = [name];
   yaml.jobs.build.strategy.matrix.ghc = supported;
   fs.writeFileSync(file, YAML.stringify(yaml));
 }
