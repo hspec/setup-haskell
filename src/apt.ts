d87e4aef6eab86330bc8d7a53c174ac97245cd8b
Simon Hengel <sol@typeful.net>
2023-03-15 17:33:03 +0700

    Remove ubuntu-18.04

--- src/apt.ts
+++ src/apt.ts
@@ -11,7 +11,7 @@ export async function install(version: string) {
 
 export function versions(): Set<string> {
   const image = process.env['ImageOS'];
-  if (image == 'ubuntu20' || image == 'ubuntu18') {
+  if (image == 'ubuntu20') {
     return ppa[image];
   } else {
     return new Set();
@@ -19,37 +19,6 @@ export function versions(): Set<string> {
 }
 
 export const ppa = {
-  ubuntu18: new Set([
-    // https://launchpad.net/~hvr/+archive/ubuntu/ghc?field.series_filter=bionic
-    '7.0.1',
-    '7.0.4',
-    '7.2.2',
-    '7.4.1',
-    '7.4.2',
-    '7.6.3',
-    '7.8.4',
-    '7.10.3',
-    '8.0.2',
-    '8.2.2',
-    '8.4.1',
-    '8.4.2',
-    '8.4.3',
-    '8.4.4',
-    '8.6.1',
-    '8.6.2',
-    '8.6.3',
-    '8.6.4',
-    '8.6.5',
-    '8.8.1',
-    '8.8.2',
-    '8.8.3',
-    '8.8.4',
-    '8.10.1',
-    '8.10.2',
-    '8.10.3',
-    '8.10.4',
-    '9.0.1',
-  ]),
   ubuntu20: new Set([
     // https://launchpad.net/~hvr/+archive/ubuntu/ghc?field.series_filter=focal
     '7.0.1',
